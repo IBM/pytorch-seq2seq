e706df751b59c79abb153809d0fd813366ab9726
kylegao91 <kyle.ygao@gmail.com>
2017-08-09 13:29:43 -0400

    Upgrade to pytorch-0.2

--- tests/test_encoder_rnn.py
+++ tests/test_encoder_rnn.py
@@ -1,6 +1,8 @@
 import os
 import unittest
 
+import torch
+
 from seq2seq.dataset import Dataset
 from seq2seq.models import EncoderRNN
 
@@ -19,7 +21,7 @@ class TestEncoderRNN(unittest.TestCase):
         batch = [[1,2,3], [1,2], [1]]
         output1, _ = rnn(batch)
         output2, _ = rnn(batch)
-        self.assertEqual(output1, output2)
+        self.assertTrue(torch.equal(output1[0].data, output2[0].data))
 
     def test_input_dropout_WITH_NON_ZERO_PROB(self):
         rnn = EncoderRNN(self.dataset.input_vocab, 50, 16, input_dropout_p=0.5)
@@ -31,7 +33,7 @@ class TestEncoderRNN(unittest.TestCase):
         for _ in range(50):
             output1, _ = rnn(batch)
             output2, _ = rnn(batch)
-            if output1 != output2:
+            if not torch.equal(output1[0].data, output2[0].data):
                 equal = False
                 break
         self.assertFalse(equal)
@@ -43,10 +45,10 @@ class TestEncoderRNN(unittest.TestCase):
         batch = [[1,2,3], [1,2], [1]]
         output1, _ = rnn(batch)
         output2, _ = rnn(batch)
-        self.assertEqual(output1, output2)
+        self.assertTrue(torch.equal(output1[0].data, output2[0].data))
 
     def test_dropout_WITH_NON_ZERO_PROB(self):
-        rnn = EncoderRNN(self.dataset.input_vocab, 50, 16, dropout_p=0.5)
+        rnn = EncoderRNN(self.dataset.input_vocab, 50, 16, n_layers=2, dropout_p=0.5)
         for param in rnn.parameters():
             param.data.uniform_(-1, 1)
         batch = [[1,2,3], [1,2], [1]]
@@ -55,7 +57,7 @@ class TestEncoderRNN(unittest.TestCase):
         for _ in range(50):
             output1, _ = rnn(batch)
             output2, _ = rnn(batch)
-            if output1 != output2:
+            if not torch.equal(output1[0].data, output2[0].data):
                 equal = False
                 break
         self.assertFalse(equal)
