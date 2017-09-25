6ba44075ffc4ded61a13f1ad1be5ee371fbe0ce6
kylegao91 <kyle.ygao@gmail.com>
2017-09-25 09:36:17 -0400

    Updated version.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.3',
+    version='0.1.4',
 
     description='A framework for sequence-to-sequence (seq2seq) models implemented in PyTorch.',
     long_description=long_description,
@@ -44,6 +44,7 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'
     ],
 
     # What does your project relate to?
