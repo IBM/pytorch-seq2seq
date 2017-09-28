62730421b519c85046db7203b42b318bb271ecbd
Kyle Gao <kyle.gao@ibm.com>
2017-09-27 21:26:55 -0400

    Fix #83

--- seq2seq/util/checkpoint.py
+++ seq2seq/util/checkpoint.py
@@ -94,6 +94,7 @@ class Checkpoint(object):
         print("Loading checkpoints from {}".format(path))
         resume_checkpoint = torch.load(os.path.join(path, cls.TRAINER_STATE_NAME))
         model = torch.load(os.path.join(path, cls.MODEL_NAME))
+        model.flatten_parameters() # make RNN parameters contiguous
         with open(os.path.join(path, cls.INPUT_VOCAB_FILE), 'rb') as fin:
             input_vocab = dill.load(fin)
         with open(os.path.join(path, cls.OUTPUT_VOCAB_FILE), 'rb') as fin:
