82ed8835fc2e7495f683519125cfb16737cb02c5
kylegao91 <kyle.ygao@gmail.com>
2017-10-01 07:58:14 -0400

    Fixed topk decoder.

--- seq2seq/models/TopKDecoder.py
+++ seq2seq/models/TopKDecoder.py
@@ -4,7 +4,7 @@ from torch.autograd import Variable
 from .baseRNN import BaseRNN
 
 
-class TopKDecoder(BaseRNN):
+class TopKDecoder(torch.nn.Module):
     r"""
     Top-K decoding with beam search.
 
@@ -37,47 +37,45 @@ class TopKDecoder(BaseRNN):
     """
 
     def __init__(self, decoder_rnn, k):
-        super(TopKDecoder, self).__init__(decoder_rnn.lang, decoder_rnn.max_length)
+        super(TopKDecoder, self).__init__()
         self.rnn = decoder_rnn
         self.k = k
-        self.V = self.rnn.lang.get_vocab_size()
-        self.SOS = self.rnn.lang.SOS_token_id
-        self.EOS = self.rnn.lang.EOS_token_id
+        self.hidden_size = self.rnn.hidden_size
+        self.V = self.rnn.output_size
+        self.SOS = self.rnn.sos_id
+        self.EOS = self.rnn.eos_id
 
-    def forward_rnn(self, inputs=None, encoder_hidden=None, encoder_outputs=None, function=F.log_softmax,
-                    retain_output_probs=True):
+    def forward(self, inputs=None, encoder_hidden=None, encoder_outputs=None, function=F.log_softmax,
+                    teacher_forcing_ratio=0, retain_output_probs=True):
         """
         Forward rnn for MAX_LENGTH steps.  Look at :func:`seq2seq.models.DecoderRNN.DecoderRNN.forward_rnn` for details.
         """
 
-        # TODO: Looks like encoder_hidden is not optional, we need unit tests
-        # for this class
-        # Get batch size, assuming h_0 is num_layers*directions x b x hidden_dim
-        b = encoder_hidden.size(1)
-        h = encoder_hidden.size(2)
+        inputs, batch_size, max_length = self.rnn._validate_args(inputs, encoder_hidden, encoder_outputs,
+                                                                 function, teacher_forcing_ratio)
 
-        self.pos_index = Variable(torch.LongTensor(range(b)) * self.k).view(-1, 1)
+        self.pos_index = Variable(torch.LongTensor(range(batch_size)) * self.k).view(-1, 1)
 
         # Inflate the initial hidden states to be of size: b*k x h
-        hidden = self._inflate(encoder_hidden, self.k)
+        if encoder_hidden is None:
+            hidden = None
+        else:
+            hidden = self._inflate(encoder_hidden, self.k)
         # ... same idea for encoder_outputs and decoder_outputs
         if self.rnn.use_attention:
-            if encoder_outputs is None:
-                raise ValueError("Argument encoder_outputs cannot be None when attention is used.")
-            else:
-                inflated_encoder_outputs = self._inflate(encoder_outputs, self.k)
+            inflated_encoder_outputs = self._inflate(encoder_outputs, self.k)
         else:
             inflated_encoder_outputs = None
 
         # Initialize the scores; for the first step,
         # ignore the inflated copies to avoid duplicate entries in the top k
-        sequence_scores = torch.Tensor(b*self.k, 1)
+        sequence_scores = torch.Tensor(batch_size * self.k, 1)
         sequence_scores.fill_(-float('Inf'))
-        sequence_scores.index_fill_(0, torch.LongTensor([i*self.k for i in range(0, b)]), 0.0)
+        sequence_scores.index_fill_(0, torch.LongTensor([i * self.k for i in range(0, batch_size)]), 0.0)
         sequence_scores = Variable(sequence_scores)
 
         # Initialize the input vector
-        input_var = Variable(torch.transpose(torch.LongTensor([[self.SOS]*b*self.k]), 0, 1))
+        input_var = Variable(torch.transpose(torch.LongTensor([[self.SOS] * batch_size * self.k]), 0, 1))
 
         # Store decisions for backtracking
         stored_outputs = list()
@@ -86,10 +84,11 @@ class TopKDecoder(BaseRNN):
         stored_emitted_symbols = list()
         stored_hidden = list()
 
-        for _ in range(0, self.rnn.max_length):
+        for _ in range(0, max_length):
 
             # Run the RNN one step forward
-            log_softmax_output, hidden, _ = self.rnn.forward_step(input_var, hidden, inflated_encoder_outputs, function=function)
+            log_softmax_output, hidden, _ = self.rnn.forward_step(input_var, hidden,
+                                                                  inflated_encoder_outputs, function=function)
 
             # If doing local backprop (e.g. supervised training), retain the output layer
             if retain_output_probs:
@@ -97,15 +96,15 @@ class TopKDecoder(BaseRNN):
 
             # To get the full sequence scores for the new candidates, add the local scores for t_i to the predecessor scores for t_(i-1)
             sequence_scores = self._inflate(sequence_scores, self.V)
-            sequence_scores += log_softmax_output
-            scores, candidates = sequence_scores.view(b, -1).topk(self.k, dim=1)
+            sequence_scores += log_softmax_output.squeeze(1)
+            scores, candidates = sequence_scores.view(batch_size, -1).topk(self.k, dim=1)
 
             # Reshape input = (bk, 1) and sequence_scores = (bk, 1)
-            input_var = (candidates % self.V).view(b * self.k, 1)
-            sequence_scores = scores.view(b * self.k, 1)
+            input_var = (candidates % self.V).view(batch_size * self.k, 1)
+            sequence_scores = scores.view(batch_size * self.k, 1)
 
             # Update fields for next timestep
-            predecessors = (candidates / self.V + self.pos_index.expand_as(candidates)).view(b*self.k, 1)
+            predecessors = (candidates / self.V + self.pos_index.expand_as(candidates)).view(batch_size * self.k, 1)
             hidden = hidden.index_select(1, predecessors.squeeze())
 
             # Update sequence scores and erase scores for end-of-sentence symbol so that they aren't expanded
@@ -121,7 +120,8 @@ class TopKDecoder(BaseRNN):
 
         # Do backtracking to return the optimal values
         output, h_t, h_n, s, l, p = self._backtrack(stored_outputs, stored_hidden,
-                                                 stored_predecessors, stored_emitted_symbols, stored_scores, b, h)
+                                                    stored_predecessors, stored_emitted_symbols,
+                                                    stored_scores, batch_size, self.hidden_size)
 
         # Build return objects
         decoder_outputs = [step[:, 0, :] for step in output]
