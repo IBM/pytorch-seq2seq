3ca1decd65d053427eaf484ec8fb7df84429e89d
kylegao91 <kyle.ygao@gmail.com>
2017-10-05 04:48:16 -0400

    Use torchtext from pipe.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'torch'],
+    install_requires=['numpy', 'torch', 'torchtext'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
