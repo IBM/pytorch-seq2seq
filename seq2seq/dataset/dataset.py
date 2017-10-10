56659341608ae7d110778d086567c7d8d8e1e238
kylegao91 <kyle.ygao@gmail.com>
2017-10-10 08:42:22 -0400

    Added index field for indexing.

--- seq2seq/dataset/dataset.py
+++ seq2seq/dataset/dataset.py
@@ -1,5 +1,6 @@
 import codecs
 
+import torch
 import torchtext
 
 from . import SourceField, TargetField
@@ -25,12 +26,18 @@ class Seq2SeqDataset(torchtext.data.Dataset):
         fields = [(src_field_name, src_field)]
         if tgt_path is not None:
             fields.append((tgt_field_name, tgt_field))
+        idx_field = torchtext.data.Field(use_vocab=False,
+                                         tensor_type=torch.LongTensor,
+                                         sequential=False)
+        fields.append(('index', idx_field))
 
         # construct examples
         corpus = _read_corpus(src_path)
         if tgt_path is not None:
             corpus = zip(corpus, _read_corpus(tgt_path))
-        examples = [torchtext.data.Example.fromlist(data, fields) for data in corpus]
+
+        examples = [torchtext.data.Example.fromlist(list(data) + [i], fields)
+                    for i, data in enumerate(corpus)]
 
         super(Seq2SeqDataset, self).__init__(examples, fields, **kwargs)
 
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,6 +1,8 @@
 import os
 import unittest
 
+import torchtext
+
 import seq2seq
 from seq2seq.dataset import Seq2SeqDataset
 
@@ -12,13 +14,22 @@ class TestDataset(unittest.TestCase):
 
     def test_init_ONLY_SRC(self):
         dataset = Seq2SeqDataset(self.src_path)
-        self.assertEqual(len(dataset.fields), 1)
+        self.assertEqual(len(dataset.fields), 2)
         self.assertEqual(len(dataset), 100)
         self.assertTrue(hasattr(dataset.examples[0], seq2seq.src_field_name))
 
     def test_init_SRC_AND_TGT(self):
         dataset = Seq2SeqDataset(self.src_path, self.tgt_path)
-        self.assertEqual(len(dataset.fields), 2)
+        self.assertEqual(len(dataset.fields), 3)
         self.assertEqual(len(dataset), 100)
         ex = dataset.examples[0]
         self.assertTrue(len(getattr(ex, seq2seq.tgt_field_name)) > 2)
+
+    def test_indices(self):
+        dataset = Seq2SeqDataset(self.src_path, self.tgt_path)
+        dataset.build_vocab(1000, 1000)
+        batch_size = 25
+
+        generator = torchtext.data.BucketIterator(dataset, batch_size, device=-1)
+        batch = next(generator.__iter__())
+        self.assertTrue(hasattr(batch, 'index'))
