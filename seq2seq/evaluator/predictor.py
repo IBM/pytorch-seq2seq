43b1ea62b93d3ed762a00df24c768a3041fa6f22
Juan-Cruz <juan.cruz@ibm.com>
2018-01-18 18:26:13 +0100

    Moving redundant code to the get_model_features function and minor fixes

--- seq2seq/evaluator/predictor.py
+++ seq2seq/evaluator/predictor.py
@@ -21,6 +21,15 @@ class Predictor(object):
         self.src_vocab = src_vocab
         self.tgt_vocab = tgt_vocab
 
+    def get_model_features(self, src_seq):
+        src_id_seq = Variable(torch.LongTensor([self.src_vocab.stoi[tok] for tok in src_seq]),
+                              volatile=True).view(1, -1)
+        if torch.cuda.is_available():
+            src_id_seq = src_id_seq.cuda()
+
+        softmax_list, _, other = self.model(src_id_seq, [len(src_seq)])
+
+        return src_id_seq, softmax_list, _, other
 
     def predict(self, src_seq):
         """ Make prediction given `src_seq` as input.
@@ -32,19 +41,15 @@ class Predictor(object):
             tgt_seq (list): list of tokens in target language as predicted
             by the pre-trained model
         """
-        src_id_seq = Variable(torch.LongTensor([self.src_vocab.stoi[tok] for tok in src_seq]),
-                              volatile=True).view(1, -1)
-        if torch.cuda.is_available():
-            src_id_seq = src_id_seq.cuda()
+        src_id_seq, softmax_list, _, other = self.get_model_features(src_seq)
 
-        softmax_list, _, other = self.model(src_id_seq, [len(src_seq)])
         length = other['length'][0]
 
         tgt_id_seq = [other['sequence'][di][0].data[0] for di in range(length)]
         tgt_seq = [self.tgt_vocab.itos[tok] for tok in tgt_id_seq]
         return tgt_seq
 
-    def predict_n(self, src_seq, n=None):
+    def predict_n(self, src_seq, n=1):
         """ Make 'n' predictions given `src_seq` as input.
 
         Args:
@@ -56,12 +61,7 @@ class Predictor(object):
             tgt_seq (list): list of tokens in target language as predicted
                             by the pre-trained model
         """
-        src_id_seq = Variable(torch.LongTensor([self.src_vocab.stoi[tok] for tok in src_seq]),
-                              volatile=True).view(1, -1)
-        if torch.cuda.is_available():
-            src_id_seq = src_id_seq.cuda()
-
-        softmax_list, _, other = self.model(src_id_seq, [len(src_seq)])
+        src_id_seq, softmax_list, _, other = self.get_model_features(src_seq)
 
         result = []
         for x in range(0, int(n)):
