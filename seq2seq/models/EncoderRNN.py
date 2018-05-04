fede87655ddce6c94b38886089e05321dc9802af
Kyle Gao <kyle.ygao@gmail.com>
2018-05-04 11:26:58 -0400

    Release 0.1.6 (#137)
    * Modified parameter order of DecoderRNN.forward (#85)
    
    * Updated TopKDecoder (#86)
    
    * Fixed topk decoder.
    
    * Use torchtext from pipy (#87)
    
    * Use torchtext from pipe.
    
    * Fixed torch text sorting order.
    
    * attention is not required when only using teacher forcing in decoder (#90)
    
    * attention is not required when only using teacher forcing in decoder
    
    * Updated docs and version.
    
    * Fixed code style.
    
    * bugfix (#92)
    
    Fixed field arguments validation.
    
    * Removed `initial_lr` when resuming optimizer with scheduler. (#95)
    
    * shuffle the training data (#97)
    
    * 0.1.5 (#91)
    
    * Modified parameter order of DecoderRNN.forward (#85)
    
    * Updated TopKDecoder (#86)
    
    * Fixed topk decoder.
    
    * Use torchtext from pipy (#87)
    
    * Use torchtext from pipe.
    
    * Fixed torch text sorting order.
    
    * attention is not required when only using teacher forcing in decoder (#90)
    
    * attention is not required when only using teacher forcing in decoder
    
    * Updated docs and version.
    
    * Fixed code style.
    
    * shuffle the training data
    
    * fix example of inflate function in TopKDecoer.py (#98)
    
    * fix example of inflate function in TopKDecoer.py
    
    * Fix hidden_layer size for one-directional decoder (#99)
    
    * Fix hidden_layer size for one-directional decoder
    
    Hidden layer size of the decoder was given `hidden_size * 2 if bidirectional else 1`, resulting in a dimensionality error for non-bidirectional decoders.
    Changed `1` to `hidden_size`.
    
    * Adapt load to allow CPU loading of GPU models (#100)
    
    * Adapt load to allow CPU loading of GPU models
    
    Add storage parameter to torch.load to allow loading
    models on a CPU that are trained on the GPU, depending
    on availability of cuda.
    
    * Fix wrong parameter use on DecoderRNN (#103)
    
    * Fix wrong parameter use on DecoderRNN
    
    * Upgrade to pytorch-0.3.0 (#111)
    
    * Upgrade to pytorch-0.3.0
    
    * Use pytorch 3.0 in travis env.
    
    * Make sure tensor contiguous when attention's not used. (#112)
    
    * Implementing the predict_n method. Using the beam search outputs it returns several seqs for a given seq (#116)
    
    * Adding a predictor method to return n predicted seqs for a src_seq input
    (intended to be used along to Beam Search using TopKDecoder)
    
    * Checkpoint after batches not epochs (#119)
    
    * Pytorch 0.4 (#134)
    
    * add contiguous call to tensor (#127)
    
    when attention is turned off, pytorch (well, 0.4 at least) gets angry about calling view on a non-contiguous tensor
    
    * Fixed shape documentation (#131)
    
    * Update to pytorch-0.4
    
    * Remove pytorch manual install in travis.
    
    * Allow using pre-trained embedding (#135)
    
    * updated docs

--- seq2seq/models/EncoderRNN.py
+++ seq2seq/models/EncoderRNN.py
@@ -16,12 +16,16 @@ class EncoderRNN(BaseRNN):
         bidirectional (bool, optional): if True, becomes a bidirectional encodr (defulat False)
         rnn_cell (str, optional): type of RNN cell (default: gru)
         variable_lengths (bool, optional): if use variable length RNN (default: False)
+        embedding (torch.Tensor, optional): Pre-trained embedding.  The size of the tensor has to match
+            the size of the embedding parameter: (vocab_size, hidden_size).  The embedding layer would be initialized
+            with the tensor if provided (default: None).
+        update_embedding (bool, optional): If the embedding should be updated during training (default: False).
 
     Inputs: inputs, input_lengths
         - **inputs**: list of sequences, whose length is the batch size and within which each sequence is a list of token IDs.
         - **input_lengths** (list of int, optional): list that contains the lengths of sequences
             in the mini-batch, it must be provided when using variable length RNN (default: `None`)
-            
+
     Outputs: output, hidden
         - **output** (batch, seq_len, hidden_size): tensor containing the encoded features of the input sequence
         - **hidden** (num_layers * num_directions, batch, hidden_size): tensor containing the features in the hidden state `h`
@@ -34,13 +38,17 @@ class EncoderRNN(BaseRNN):
     """
 
     def __init__(self, vocab_size, max_len, hidden_size,
-            input_dropout_p=0, dropout_p=0,
-            n_layers=1, bidirectional=False, rnn_cell='gru', variable_lengths=False):
+                 input_dropout_p=0, dropout_p=0,
+                 n_layers=1, bidirectional=False, rnn_cell='gru', variable_lengths=False,
+                 embedding=None, update_embedding=True):
         super(EncoderRNN, self).__init__(vocab_size, max_len, hidden_size,
                 input_dropout_p, dropout_p, n_layers, rnn_cell)
 
         self.variable_lengths = variable_lengths
         self.embedding = nn.Embedding(vocab_size, hidden_size)
+        if embedding is not None:
+            self.embedding.weight = nn.Parameter(embedding)
+        self.embedding.weight.requires_grad = update_embedding
         self.rnn = self.rnn_cell(hidden_size, hidden_size, n_layers,
                                  batch_first=True, bidirectional=bidirectional, dropout=dropout_p)
 
