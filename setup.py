ee7d6bdbdc8fad739b93522835ca0015217a023a
kylegao91 <kyle.ygao@gmail.com>
2018-05-04 11:14:16 -0400

    updated docs

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.5',
+    version='0.1.6',
 
     description='A framework for sequence-to-sequence (seq2seq) models implemented in PyTorch.',
     long_description=long_description,
