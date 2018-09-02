ca800520193942a73d23cd2b36975c5a9d6d3150
Prabhsimran Singh <pskrunner14@gmail.com>
2018-09-02 18:41:35 +0530

    modified topkdecoder test for more batch sizes

--- tests/test_topkdecoder.py
+++ tests/test_topkdecoder.py
@@ -13,8 +13,8 @@ class TestDecoderRNN(unittest.TestCase):
         self.vocab_size = 3
 
     def test_init(self):
-        decoder = DecoderRNN(self.vocab_size, 50, 16, 0, 1, input_dropout_p=0)
-        TopKDecoder(decoder, 3)
+        decoder = DecoderRNN(self.vocab_size, 50, 16, 0, 1, input_dropout_p=0).to('cpu')
+        TopKDecoder(decoder, 3).to('cpu')
 
     def test_k_1(self):
         """ When k=1, the output of topk decoder should be the same as a normal decoder. """
@@ -53,93 +53,94 @@ class TestDecoderRNN(unittest.TestCase):
                 if sum(finished) == batch_size:
                     break
 
-    def test_k_greater_then_1(self):
+    def test_k_greater_than_1(self):
         """ Implement beam search manually and compare results from topk decoder. """
         max_len = 50
-        beam_size = 3
         batch_size = 1
         hidden_size = 8
         sos = 0
         eos = 1
 
-        for _ in range(10):
-            decoder = DecoderRNN(self.vocab_size, max_len, hidden_size, sos, eos)
-            for param in decoder.parameters():
-                param.data.uniform_(-1, 1)
-            topk_decoder = TopKDecoder(decoder, beam_size)
+        for beam_size in range(2, 15, 3):
+            for batch_size in range(1, 50, 10):
+                for _ in range(10):
+                    decoder = DecoderRNN(self.vocab_size, max_len, hidden_size, sos, eos).to('cpu')
+                    for param in decoder.parameters():
+                        param.data.uniform_(-1, 1)
+                    topk_decoder = TopKDecoder(decoder, beam_size).to('cpu')
 
-            encoder_hidden = torch.autograd.Variable(torch.randn(1, batch_size, hidden_size))
-            _, hidden_topk, other_topk = topk_decoder(None, encoder_hidden=encoder_hidden)
+                    encoder_hidden = torch.autograd.Variable(torch.randn(1, batch_size, hidden_size))
+                    _, hidden_topk, other_topk = topk_decoder(None, encoder_hidden=encoder_hidden)
 
-            # Queue state:
-            #   1. time step
-            #   2. symbol
-            #   3. hidden state
-            #   4. accumulated log likelihood
-            #   5. beam number
-            batch_queue = [[(-1, sos, encoder_hidden[:,b,:].unsqueeze(1), 0, None)] for b in range(batch_size)]
-            time_batch_queue = [batch_queue]
-            batch_finished_seqs = [list() for _ in range(batch_size)]
-            for t in range(max_len):
-                new_batch_queue = []
-                for b in range(batch_size):
-                    new_queue = []
-                    for k in range(min(len(time_batch_queue[t][b]), beam_size)):
-                        _, inputs, hidden, seq_score, _ = time_batch_queue[t][b][k]
-                        if inputs == eos:
-                            batch_finished_seqs[b].append(time_batch_queue[t][b][k])
-                            continue
-                        inputs = torch.autograd.Variable(torch.LongTensor([[inputs]]))
-                        context, hidden, attn = decoder.forward_step(inputs, hidden, None)
-                        decoder_outputs, symbols = decoder.decoder(context, attn, None, None)
-                        decoder_outputs = decoder_outputs.log()
-                        topk_score, topk = decoder_outputs[0].data.topk(beam_size)
-                        for score, sym in zip(topk_score.tolist()[0], topk.tolist()[0]):
-                            new_queue.append((t, sym, hidden, score + seq_score, k))
-                    new_queue = sorted(new_queue, key=lambda x: x[3], reverse=True)[:beam_size]
-                    new_batch_queue.append(new_queue)
-                time_batch_queue.append(new_batch_queue)
+                    # Queue state:
+                    #   1. time step
+                    #   2. symbol
+                    #   3. hidden state
+                    #   4. accumulated log likelihood
+                    #   5. beam number
+                    batch_queue = [[(-1, sos, encoder_hidden[:,b,:].unsqueeze(1), 0, None)] for b in range(batch_size)]
+                    time_batch_queue = [batch_queue]
+                    batch_finished_seqs = [list() for _ in range(batch_size)]
+                    for t in range(max_len):
+                        new_batch_queue = []
+                        for b in range(batch_size):
+                            new_queue = []
+                            for k in range(min(len(time_batch_queue[t][b]), beam_size)):
+                                _, inputs, hidden, seq_score, _ = time_batch_queue[t][b][k]
+                                if inputs == eos:
+                                    batch_finished_seqs[b].append(time_batch_queue[t][b][k])
+                                    continue
+                                inputs = torch.autograd.Variable(torch.LongTensor([[inputs]]))
+                                context, hidden, attn = decoder.forward_step(inputs, hidden, None)
+                                decoder_outputs, symbols = decoder.decoder(context, attn, None, None)
+                                decoder_outputs = decoder_outputs.log()
+                                topk_score, topk = decoder_outputs[0].data.topk(beam_size)
+                                for score, sym in zip(topk_score.tolist()[0], topk.tolist()[0]):
+                                    new_queue.append((t, sym, hidden, score + seq_score, k))
+                            new_queue = sorted(new_queue, key=lambda x: x[3], reverse=True)[:beam_size]
+                            new_batch_queue.append(new_queue)
+                        time_batch_queue.append(new_batch_queue)
 
-            # finished beams
-            finalist = [l[:beam_size] for l in batch_finished_seqs]
-            # unfinished beams
-            for b in range(batch_size):
-                if len(finalist[b]) < beam_size:
-                    last_step = sorted(time_batch_queue[-1][b], key=lambda x: x[3], reverse=True)
-                    finalist[b] += last_step[:beam_size - len(finalist[b])]
+                    # finished beams
+                    finalist = [l[:beam_size] for l in batch_finished_seqs]
+                    # unfinished beams
+                    for b in range(batch_size):
+                        if len(finalist[b]) < beam_size:
+                            last_step = sorted(time_batch_queue[-1][b], key=lambda x: x[3], reverse=True)
+                            finalist[b] += last_step[:beam_size - len(finalist[b])]
 
-            # back track
-            topk = []
-            for b in range(batch_size):
-                batch_topk = []
-                for k in range(beam_size):
-                    seq = [finalist[b][k]]
-                    prev_k = seq[-1][4]
-                    prev_t = seq[-1][0]
-                    while prev_k is not None:
-                        seq.append(time_batch_queue[prev_t][b][prev_k])
-                        prev_k = seq[-1][4]
-                        prev_t = seq[-1][0]
-                    batch_topk.append([s for s in reversed(seq)])
-                topk.append(batch_topk)
+                    # back track
+                    topk = []
+                    for b in range(batch_size):
+                        batch_topk = []
+                        for k in range(beam_size):
+                            seq = [finalist[b][k]]
+                            prev_k = seq[-1][4]
+                            prev_t = seq[-1][0]
+                            while prev_k is not None:
+                                seq.append(time_batch_queue[prev_t][b][prev_k])
+                                prev_k = seq[-1][4]
+                                prev_t = seq[-1][0]
+                            batch_topk.append([s for s in reversed(seq)])
+                        topk.append(batch_topk)
 
-            for b in range(batch_size):
-                topk[b] = sorted(topk[b], key=lambda s: s[-1][3], reverse=True)
+                    for b in range(batch_size):
+                        topk[b] = sorted(topk[b], key=lambda s: s[-1][3], reverse=True)
 
-            topk_scores = other_topk['score']
-            topk_lengths = other_topk['topk_length']
-            topk_pred_symbols = other_topk['topk_sequence']
-            for b in range(batch_size):
-                precision_error = False
-                for k in range(beam_size - 1):
-                    if np.isclose(topk_scores[b][k], topk_scores[b][k+1]):
-                        precision_error = True
-                        break
-                if precision_error:
-                    break
-                for k in range(beam_size):
-                    self.assertEqual(topk_lengths[b][k], len(topk[b][k]) - 1)
-                    self.assertTrue(np.isclose(topk_scores[b][k], topk[b][k][-1][3]))
-                    total_steps = topk_lengths[b][k]
-                    for t in range(total_steps):
-                        self.assertEqual(topk_pred_symbols[t][b, k].data[0], topk[b][k][t+1][1]) # topk includes SOS
+                    topk_scores = other_topk['score']
+                    topk_lengths = other_topk['topk_length']
+                    topk_pred_symbols = other_topk['topk_sequence']
+                    for b in range(batch_size):
+                        precision_error = False
+                        for k in range(beam_size - 1):
+                            if np.isclose(topk_scores[b][k], topk_scores[b][k+1]):
+                                precision_error = True
+                                break
+                        if precision_error:
+                            break
+                        for k in range(beam_size):
+                            self.assertEqual(topk_lengths[b][k], len(topk[b][k]) - 1)
+                            self.assertTrue(np.isclose(topk_scores[b][k], topk[b][k][-1][3]))
+                            total_steps = topk_lengths[b][k]
+                            for t in range(total_steps):
+                                self.assertEqual(topk_pred_symbols[t][b, k].data[0], topk[b][k][t+1][1]) # topk includes SOS
